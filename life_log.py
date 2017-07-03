533f556d9bf489820ea4459567300e189c643243
Shinsuke-Abe <mao.instantlife@gmail.com>
2017-07-04 05:43:00 +0900

    入力のチェックとHTTPでのリクエストを想定したアプリケーションエラーハンドリング

--- life_log.py
+++ life_log.py
@@ -2,15 +2,27 @@ import json
 
 
 def create_life_log(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function named create_life_log "
-                   "executed successfully(separated python file)!",
-        "input": event
-    }
+    try:
+        id_value = event["id"]
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+        print(id_value)
+
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Go Serverless v1.0! "
+                           "Your function named create_life_log "
+                           "executed successfully(separated python file)!",
+                "input": event
+            })
+        }
+    except KeyError:
+        response = {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Your function named create_life_log executed,"
+                           "but not found required input!"
+            })
+        }
 
     return response
