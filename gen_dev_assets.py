fe60f77ff9130f89810713df75c0820531bafd46
Shinsuke-Abe <mao.instantlife@gmail.com>
2017-07-14 20:28:38 +0900

    ステージされたAPI情報を取得

--- gen_dev_assets.py
+++ gen_dev_assets.py
@@ -4,5 +4,11 @@ import os
 
 
 def generate_swagger(event, context):
-    print(os.environ['REST_API_NAME'])
+    client = boto3.client('apigateway')
+
+    staged_apis = [api for api in client.get_rest_apis()['items']
+                   if api['name'] == os.environ['REST_API_NAME']]
+
+    print(staged_apis.pop())
+
     return
