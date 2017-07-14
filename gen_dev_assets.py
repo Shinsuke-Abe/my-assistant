83831732b2c9f3e1c9262a3f3e200ba62c4a3622
Shinsuke-Abe <mao.instantlife@gmail.com>
2017-07-14 20:59:45 +0900

    エクスポートしたAPIをswagger.jsonとしてS3に保存する

--- gen_dev_assets.py
+++ gen_dev_assets.py
@@ -1,20 +1,27 @@
-import json
 import boto3
 import os
 
+apigateway = boto3.client('apigateway')
+s3 = boto3.resource('s3')
+
 
 def generate_swagger(event, context):
-    client = boto3.client('apigateway')
-
-    staged_api = [api for api in client.get_rest_apis()['items']
+    staged_api = [api for api in apigateway.get_rest_apis()['items']
                   if api['name'] == os.environ['REST_API_NAME']].pop()
 
-    exported_api = client.get_export(
+    exported_api = apigateway.get_export(
         restApiId=staged_api['id'],
         stageName=os.environ['REST_API_STAGE_NAME'],
         exportType="swagger"
     )
 
-    print(exported_api)
+    swagger_bucket = s3.Bucket(os.environ['SWAGGER_FILE_BUCKET'])
+
+    swagger_file = swagger_bucket.Object('swagger.json')
+    swagger_file.put(
+        Body=exported_api['body'].read().decode('utf-8'),
+        ContentEncoding='utf-8',
+        ContentType='text/plane'
+    )
 
     return
